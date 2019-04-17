889f5d4dffb267e0748195486fc8d48be277efc3
Ivan Suslov <ioankingisepp@mail.ru>
2019-04-17 08:01:44 +0300

    5. Пишем первую функцию

--- index.php
+++ index.php
@@ -50,6 +50,14 @@ $adverts = [
         'url' => 'img/lot-6.jpg',
     ],
 ];
+
+function formattingAmount($number) {
+    $rounded_number = ceil($number);
+    if ($rounded_number <= 1000) {
+        return $rounded_number." ₽";
+    }
+    return number_format($rounded_number, 0, '', ' ')." ₽";
+};
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -126,7 +134,7 @@ $adverts = [
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value['price']?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=formattingAmount($value['price'])?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
