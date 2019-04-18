f988501d1335458bdc2479d64d3ff6ac6271f521
Ivan Suslov <ioankingisepp@mail.ru>
2019-04-18 12:10:20 +0300

    7. Шаблонизация проекта

---
+++ inc/config.php
@@ -0,0 +1,4 @@
+<?php
+$is_auth = rand(0, 1);
+$title_page = 'YetiCave';
+$user_name = 'Иван Суслов';
---
+++ templates/index.php
@@ -0,0 +1,39 @@
+<section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <!--заполните этот список из массива категорий-->
+            <?php foreach ($cathegory as $key => $value): ?>
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="pages/all-lots.html"><?=$value?></a>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+        <?php foreach ($adverts as $key => $value): ?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?=htmlspecialchars($value['url'])?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?=htmlspecialchars($value['cathegory'])?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['title'])?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?=formattingAmount($value['price'])?></span>
+                        </div>
+                        <div class="lot__timer timer">
+                            12:23
+                        </div>
+                    </div>
+                </div>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+    </section>
